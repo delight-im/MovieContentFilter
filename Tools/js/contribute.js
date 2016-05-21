b297eacb01e2304804abcf8cf2138c450e63827b
Marco <marco1@live.de>
2016-05-21 15:24:19 +0200

    Create web application for building filters conveniently via UI

---
+++ Tools/js/contribute.js
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) delight.im <info@delight.im>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+"use strict";
+
+var sourceElement = $("#video-source-hidden");
+
+var player = new MediaPlayer();
+player.setTargetElement(document.getElementById("video-target"));
+
+sourceElement.change(function () {
+	try {
+		player.play(this.files[0]);
+
+		$("#screen-intro").css("display", "none");
+		$("#screen-annotate").css("display", "");
+	}
+	catch (e) {
+		if (e instanceof MediaPlayer.InvalidInputException) {
+			alert("Invalid input: "+e.reason);
+		}
+		else if (e instanceof MediaPlayer.BrowserNotSupportedException) {
+			alert("Browser not supported: "+e.reason);
+		}
+		else if (e instanceof MediaPlayer.MissingTargetElementException) {
+			alert("Missing target element");
+		}
+		else if (e instanceof MediaPlayer.UnsupportedFormatException) {
+			alert("The format of the selected medium is not supported in your browser");
+		}
+		else {
+			alert("Unknown error");
+		}
+	}
+});
+
+$("#video-source").click(function () {
+	sourceElement.click();
+});
+
+var lastCutStart = null;
+
+var annotationControls = {
+	markStart: $("#mark-start"),
+	startCut: $("#start-cut"),
+	endCut: $("#end-cut"),
+	cancelCut: $("#cancel-cut"),
+	markEnd: $("#mark-end")
+};
+
+annotationControls.markStart.click(function () {
+	alert("start = "+player.getElapsedTime()+"s");
+	annotationControls.startCut.prop("disabled", false);
+	annotationControls.markEnd.prop("disabled", false);
+});
+annotationControls.startCut.click(function () {
+	lastCutStart = player.getElapsedTime();
+
+	annotationControls.endCut.css("display", "inline-block");
+	annotationControls.cancelCut.css("display", "inline-block");
+	annotationControls.startCut.css("display", "none");
+});
+annotationControls.endCut.click(function () {
+	alert("cut = ["+lastCutStart+"s, "+player.getElapsedTime()+"s]");
+	annotationControls.endCut.css("display", "none");
+	annotationControls.cancelCut.css("display", "none");
+	annotationControls.startCut.css("display", "inline-block");
+
+	lastCutStart = null;
+});
+annotationControls.cancelCut.click(function () {
+	lastCutStart = null;
+
+	annotationControls.endCut.css("display", "none");
+	annotationControls.cancelCut.css("display", "none");
+	annotationControls.startCut.css("display", "inline-block");
+});
+annotationControls.markEnd.click(function () {
+	alert("end = "+player.getElapsedTime()+"s");
+});
