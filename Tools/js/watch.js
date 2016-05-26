3503f8f8d94a0762cde9dfd744bfe37f499529fd
Marco <marco1@live.de>
2016-05-26 10:25:47 +0200

    Move 'saveTextToFile' function to new 'common.js'

--- Tools/js/watch.js
+++ Tools/js/watch.js
@@ -141,19 +141,6 @@ function convertFilters(outputFormat) {
 	}
 }
 
-function saveTextToFile(text, filename, mimeType) {
-	mimeType = mimeType || "text/plain";
-
-	if (!!new Blob) {
-		var contentType = mimeType+"; charset=utf-8";
-		var blob = new Blob([ text ], { type: contentType });
-		saveAs(blob, filename);
-	}
-	else {
-		throw "Saving files is not supported in your browser";
-	}
-}
-
 function createCategoryKey(category, parentCategory) {
 	if (parentCategory === null) {
 		return "preference-"+category;
