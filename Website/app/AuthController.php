8c49455bfa3b8850e0c400441114f4cb40199d8e
Marco <marco1@live.de>
2016-10-18 07:47:31 +0200

    Add method 'AuthController#sendEmail' for sending emails efficiently

--- Website/app/AuthController.php
+++ Website/app/AuthController.php
@@ -23,6 +23,55 @@ class AuthController extends Controller {
 		echo $app->view('sign-up.html');
 	}
 
+	private static function sendEmail(App $app, $subject, $template, $toAddress, $toName = null, $params = null) {
+		// since we're sending an email, this request *may* take a bit longer in some rare cases
+		set_time_limit(60);
+
+		// never stop execution before the email has been sent just because the client disconnected
+		ignore_user_abort(true);
+
+		// if no parameters have been provided
+		if ($params === null) {
+			// initialize an empty array
+			$params = [];
+		}
+
+		// add the general parameters that are passed to all email templates
+		$params['projectName'] = $_ENV['COM_MOVIECONTENTFILTER_PROJECT_NAME'];
+		$params['projectUrl'] = parse_url($_ENV['APP_PUBLIC_URL'], PHP_URL_HOST);
+		$params['projectEmail'] = $_ENV['COM_MOVIECONTENTFILTER_MAIL_REPLY_TO'];
+		$params['projectPostal'] = $_ENV['COM_MOVIECONTENTFILTER_PROJECT_POSTAL'];
+		$params['address'] = $toAddress;
+		$params['name'] = $toName;
+
+		// combine the template and the parameters into the complete body text
+		$body = $app->view($template, $params);
+
+		// create a new message object
+		/** @var \Swift_Mime_Message $obj */
+		$obj = $app->mail()->createMessage();
+
+		// configure the message
+		$obj->setSubject($subject);
+		$obj->setFrom(
+			[ $_ENV['COM_MOVIECONTENTFILTER_MAIL_FROM'] => $_ENV['COM_MOVIECONTENTFILTER_PROJECT_NAME'] ]
+		);
+		if (empty($toName)) {
+			$obj->setTo(
+				[ $toAddress ]
+			);
+		}
+		else {
+			$obj->setTo(
+				[ $toAddress => $toName ]
+			);
+		}
+		$obj->setBody($body);
+
+		// send the message and return whether this operation succeeded
+		return $app->mail()->send($obj);
+	}
+
 	public static function saveSignUp(App $app) {
 		$email = $app->input()->post('email', TYPE_EMAIL);
 		$password1 = $app->input()->post('password-1');
